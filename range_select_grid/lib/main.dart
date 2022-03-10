f7130f6f19121af878b0b19b5e3e424c74b78505
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2022-03-10 23:52:55 +0600

    gester detect

---
+++ range_select_grid/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+        color: Colors.green,
+        height: 200,
+        width: 200,
+      )),
+    );
+  }
+}
