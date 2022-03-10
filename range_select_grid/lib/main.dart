76465aece13dbef2974f220464b432ec9c49da98
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2022-03-11 01:38:02 +0600

    issue

--- range_select_grid/lib/main.dart
+++ range_select_grid/lib/main.dart
@@ -15,53 +15,146 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: GestureDetector(
-        onPanUpdate: (details) {
-          print(
-              'Offset: ${details.globalPosition.dx}, ${details.globalPosition.dy}');
-        },
-        child: Container(
+        body: Center(
+            child: GestureDetector(
+      onPanUpdate: (details) {
+        final _dx = details.globalPosition.dx;
+        final _dy = details.globalPosition.dy;
+        // print(
+        //     'Offset: ${details.globalPosition.dx}, ${details.globalPosition.dy}');
+        final _offset = offsetBoxes!.first;
+        if (_dx > _offset.xf! &&
+            _dx < _offset.xe! &&
+            _dy > _offset.yf! &&
+            _dy < _offset.ye!) {
+          print('1');
+        }
+      },
+      child: Container(
           color: Colors.green,
           height: 300,
           width: 500,
-          child: Row(children: [...keys.map((e) => Button(e)).toList()]),
-        ),
-      )),
-    );
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: keys.length,
+            itemBuilder: (context, index) {
+              Future.delayed(const Duration(seconds: 2), () {
+                print('xx');
+                final RenderBox renderBox =
+                    keys[index].currentContext?.findRenderObject() as RenderBox;
+                final Offset offset = renderBox.localToGlobal(Offset.zero);
+                final Size size =
+                    renderBox.size; // or _widgetKey.currentContext?.size
+                print('xf ${offset.dx} xe ${offset.dx + size.width}');
+                if (offsetBoxes == null) {
+                  final _data = OffsetDetails(
+                      xf: offset.dx,
+                      xe: offset.dx + size.width,
+                      yf: offset.dy,
+                      ye: offset.dy + size.height);
+                  offsetBoxes?[index] = _data;
+                  print(offsetBoxes![index]);
+                }
+              });
+
+              return Button(key: keys[index], index: index);
+            },
+          )),
+    )));
   }
 }
 
 class Button extends StatelessWidget {
-  const Button(
-    this.key,
-  ) : super(key: key);
+  const Button({
+    required this.key,
+    required this.index,
+  }) : super(key: key);
   final GlobalKey key;
+  final int index;
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('\npressed');
-        final RenderBox renderBox =
-            key.currentContext?.findRenderObject() as RenderBox;
-
-        final Size size = renderBox.size; // or _widgetKey.currentContext?.size
-
-        print('Size: ${size.width}, ${size.height}');
-
-        final Offset offset = renderBox.localToGlobal(Offset.zero);
-
-        print('Offset: ${offset.dx}, ${offset.dy}');
-
-        print(
-            'Position: ${(offset.dx + size.width) / 2}, ${(offset.dy + size.height) / 2}');
-      },
-      child: Container(
-        height: 100,
-        width: 80,
-        margin: const EdgeInsets.all(5),
-        color: Colors.orange,
-      ),
+    return Container(
+      height: 100,
+      width: 80,
+      decoration: BoxDecoration(
+          color: Colors.orange,
+          border: Border.all(width: 2, color: Colors.black)),
     );
   }
+}
+
+// class XButton extends StatelessWidget {
+//   const XButton({
+//     required this.key,
+//     required this.index,
+//   }) : super(key: key);
+//   final GlobalKey key;
+//   final int index;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         height: 100,
+//         width: 80,
+//         color: Colors.blue,
+//         child: Button(key: key, index: index));
+//   }
+// }
+
+// class Button extends StatelessWidget {
+//   const Button({
+//     required this.key,
+//     required this.index,
+//   }) : super(key: key);
+//   final GlobalKey key;
+//   final int index;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print(key);
+//     Future.delayed(const Duration(seconds: 1));
+//     final RenderBox renderBox =
+//         key.currentContext!.findRenderObject() as RenderBox;
+//     final Size size = renderBox.size;
+//     final Offset offset = renderBox.localToGlobal(Offset.zero);
+//     return GestureDetector(
+//       onTap: () {
+//         print('\npressed');
+//         final RenderBox renderBox =
+//             key.currentContext?.findRenderObject() as RenderBox;
+
+//         final Size size = renderBox.size; // or _widgetKey.currentContext?.size
+
+//         // print('Size: ${size.width}, ${size.height}');
+
+//         final Offset offset = renderBox.localToGlobal(Offset.zero);
+
+//         // print('Offset: ${offset.dx}, ${offset.dy}');
+
+//         // print(
+//         //     'Position: ${(offset.dx + size.width) / 2}, ${(offset.dy + size.height) / 2}');
+//         print('xf ${offset.dx} xe ${offset.dx + size.width}');
+//       },
+//       child: Container(
+//         height: 100,
+//         width: 80,
+//         margin: const EdgeInsets.all(5),
+//         color: Colors.orange,
+//       ),
+//     );
+//   }
+// }
+
+List<OffsetDetails>? offsetBoxes =
+    List.filled(5, OffsetDetails(xf: null, xe: null, yf: null, ye: null));
+
+class OffsetDetails {
+  final double? xf;
+  final double? xe;
+  final double? yf;
+  final double? ye;
+
+  OffsetDetails(
+      {required this.xf, required this.xe, required this.yf, required this.ye});
 }
