3e150437398b7723bb863b049fc7b0462b91974b
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2022-03-11 12:50:47 +0600

    complete with statefull widget

--- range_select_grid/lib/main.dart
+++ range_select_grid/lib/main.dart
@@ -9,7 +9,8 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-final keys = List<GlobalKey>.generate(5, (index) => GlobalKey());
+final keys = List<GlobalKey>.generate(25, (index) => GlobalKey());
+int? startIndex, endIndex, selectedIndex;
 
 class _HomeState extends State<Home> {
   @override
@@ -24,34 +25,45 @@ class _HomeState extends State<Home> {
           return _dx > e.xf! && _dx < e.xe! && _dy > e.yf! && _dy < e.ye!;
         }).toList();
         final _index = offsetBoxes!.indexOf(_start.first);
-        print('start $_index');
+
+        startIndex = _index;
+        endIndex = _index;
+        selectedIndex = null;
+        print('start $startIndex end $endIndex');
       },
       onPanUpdate: (details) {
+        late int _index;
         final _dx = details.globalPosition.dx;
         final _dy = details.globalPosition.dy;
         final _start = offsetBoxes!.where((e) {
           return _dx >= e.xf! && _dx <= e.xe! && _dy >= e.yf! && _dy <= e.ye!;
         }).toList();
-        print('.................... ${_start.length}');
-        final _index = offsetBoxes!.indexOf(_start.first);
-        print('Last $_index');
+        try {
+          if (_start.isNotEmpty) {
+            _index = offsetBoxes!.indexOf(_start.first);
+
+            endIndex = _index;
+            selectedIndex = null;
+            print('start $startIndex end $endIndex');
+          }
+        } catch (e) {
+          print(e.toString());
+        }
+        setState(() {});
       },
       child: Container(
           color: Colors.green,
-          height: 300,
-          width: 500,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
+          child: GridView.builder(
             itemCount: keys.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5),
             itemBuilder: (context, index) {
-              Future.delayed(const Duration(seconds: 2), () {
-                print('xx');
+              Future.delayed(const Duration(seconds: 1), () {
                 final RenderBox renderBox =
                     keys[index].currentContext?.findRenderObject() as RenderBox;
                 final Offset offset = renderBox.localToGlobal(Offset.zero);
-                final Size size =
-                    renderBox.size; // or _widgetKey.currentContext?.size
-                // print('xf ${offset.dx} xe ${offset.dx + size.width}');
+                final Size size = renderBox.size;
+
                 if (offsetBoxes?[index].xf == null) {
                   final _data = OffsetDetails(
                       xf: offset.dx,
@@ -62,10 +74,42 @@ class _HomeState extends State<Home> {
                 } else {}
               });
 
-              return Button(key: keys[index], index: index);
+              return InkWell(
+                onTap: () {
+                  selectedIndex = index;
+                  setState(() {});
+                },
+                child: Button(
+                  key: keys[index],
+                  index: index,
+                  isSelected: select(index),
+                ),
+              );
             },
           )),
     )));
+  }
+
+  bool select(index) {
+    if (selectedIndex == null) {
+      return startIndex != null && endIndex != null
+          ? isSelected(index: index, start: startIndex!, end: endIndex!)
+          : false;
+    }
+    return index == selectedIndex;
+  }
+
+  bool isSelected({required int index, required int start, required int end}) {
+    if (start <= end) {
+      if (index >= start && index <= end) {
+        return true;
+      }
+    } else {
+      if (index <= start && index >= end) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
@@ -73,17 +117,17 @@ class Button extends StatelessWidget {
   const Button({
     required this.key,
     required this.index,
+    required this.isSelected,
   }) : super(key: key);
   final GlobalKey key;
   final int index;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: 100,
       decoration: BoxDecoration(
-          color: Colors.orange,
+          color: isSelected ? Colors.green : Colors.orange,
           border: Border.all(width: 2, color: Colors.black)),
     );
   }
@@ -151,8 +195,8 @@ class Button extends StatelessWidget {
 //   }
 // }
 
-List<OffsetDetails>? offsetBoxes =
-    List.filled(5, OffsetDetails(xf: null, xe: null, yf: null, ye: null));
+List<OffsetDetails>? offsetBoxes = List.filled(
+    keys.length, OffsetDetails(xf: null, xe: null, yf: null, ye: null));
 
 class OffsetDetails {
   final double? xf;
