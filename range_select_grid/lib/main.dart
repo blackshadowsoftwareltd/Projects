c1353c6873fb67561270bb6dcd8d14eca9b68ed3
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2022-03-11 15:19:23 +0600

    more range selection options

--- range_select_grid/lib/main.dart
+++ range_select_grid/lib/main.dart
@@ -17,76 +17,95 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: GestureDetector(
-      onPanStart: (details) {
-        final _dx = details.globalPosition.dx;
-        final _dy = details.globalPosition.dy;
-        final _start = offsetBoxes!.where((e) {
-          return _dx > e.xf! && _dx < e.xe! && _dy > e.yf! && _dy < e.ye!;
-        }).toList();
-        final _index = offsetBoxes!.indexOf(_start.first);
+            child: SizedBox(
+      width: 400,
+      height: 400,
+      child: GestureDetector(
+        onPanStart: (details) {
+          final _dx = details.globalPosition.dx;
+          final _dy = details.globalPosition.dy;
+          final _start = offsetBoxes!.where((e) {
+            return _dx > e.xf! && _dx < e.xe! && _dy > e.yf! && _dy < e.ye!;
+          }).toList();
+          final _index = offsetBoxes!.indexOf(_start.first);
 
-        startIndex = _index;
-        endIndex = _index;
-        selectedIndex = null;
-        print('start $startIndex end $endIndex');
-      },
-      onPanUpdate: (details) {
-        late int _index;
-        final _dx = details.globalPosition.dx;
-        final _dy = details.globalPosition.dy;
-        final _start = offsetBoxes!.where((e) {
-          return _dx >= e.xf! && _dx <= e.xe! && _dy >= e.yf! && _dy <= e.ye!;
-        }).toList();
-        try {
-          if (_start.isNotEmpty) {
-            _index = offsetBoxes!.indexOf(_start.first);
+          startIndex = _index;
+          endIndex = _index;
+          selectedIndex = null;
+          print('start $startIndex end $endIndex');
+        },
+        onPanUpdate: (details) {
+          late int _index;
+          final _dx = details.globalPosition.dx;
+          final _dy = details.globalPosition.dy;
+          final _start = offsetBoxes!.where((e) {
+            return _dx >= e.xf! && _dx <= e.xe! && _dy >= e.yf! && _dy <= e.ye!;
+          }).toList();
+          try {
+            if (_start.isNotEmpty) {
+              _index = offsetBoxes!.indexOf(_start.first);
 
-            endIndex = _index;
-            selectedIndex = null;
-            print('start $startIndex end $endIndex');
+              endIndex = _index;
+              selectedIndex = null;
+              print('start $startIndex end $endIndex');
+            }
+          } catch (e) {
+            print(e.toString());
           }
-        } catch (e) {
-          print(e.toString());
-        }
-        setState(() {});
-      },
-      child: Container(
-          color: Colors.green,
-          child: GridView.builder(
-            itemCount: keys.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5),
-            itemBuilder: (context, index) {
-              Future.delayed(const Duration(seconds: 1), () {
-                final RenderBox renderBox =
-                    keys[index].currentContext?.findRenderObject() as RenderBox;
-                final Offset offset = renderBox.localToGlobal(Offset.zero);
-                final Size size = renderBox.size;
+          setState(() {});
+        },
+        child: GridView.builder(
+          itemCount: keys.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 5),
+          itemBuilder: (context, index) {
+            Future.delayed(const Duration(seconds: 1), () {
+              final RenderBox renderBox =
+                  keys[index].currentContext?.findRenderObject() as RenderBox;
+              final Offset offset = renderBox.localToGlobal(Offset.zero);
+              final Size size = renderBox.size;
 
-                if (offsetBoxes?[index].xf == null) {
-                  final _data = OffsetDetails(
-                      xf: offset.dx,
-                      xe: offset.dx + size.width,
-                      yf: offset.dy,
-                      ye: offset.dy + size.height);
-                  offsetBoxes?[index] = _data;
-                } else {}
-              });
+              if (offsetBoxes?[index].xf == null) {
+                final _data = OffsetDetails(
+                    xf: offset.dx,
+                    xe: offset.dx + size.width,
+                    yf: offset.dy,
+                    ye: offset.dy + size.height);
+                offsetBoxes?[index] = _data;
+              } else {}
+            });
 
-              return InkWell(
-                onTap: () {
+            return GestureDetector(
+              onTap: () {
+                if (selectedIndex == index) {
+                  selectedIndex = null;
+                } else if (selectedIndex == null) {
                   selectedIndex = index;
-                  setState(() {});
-                },
-                child: Button(
-                  key: keys[index],
-                  index: index,
-                  isSelected: select(index),
-                ),
-              );
-            },
-          )),
+                  startIndex = index;
+                  endIndex = null;
+                } else if (startIndex != null && endIndex == null) {
+                  selectedIndex = null;
+                  endIndex = index;
+                  selectedIndex = null;
+                } else if (startIndex == index ||
+                    endIndex == index ||
+                    selectedIndex == index) {
+                  startIndex = null;
+                  endIndex = null;
+                  selectedIndex = null;
+                }
+
+                setState(() {});
+              },
+              child: Button(
+                key: keys[index],
+                index: index,
+                isSelected: select(index),
+              ),
+            );
+          },
+        ),
+      ),
     )));
   }
 
@@ -125,75 +144,26 @@ class Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      margin: isSelected ? const EdgeInsets.all(1) : const EdgeInsets.all(3),
+      duration: const Duration(milliseconds: 200),
       decoration: BoxDecoration(
-          color: isSelected ? Colors.green : Colors.orange,
-          border: Border.all(width: 2, color: Colors.black)),
+          color: isSelected ? Colors.lightBlueAccent.shade400 : Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              width: 1,
+              color: isSelected ? Colors.transparent : Colors.black54)),
+      child: Center(
+        child: Text(
+          '${index + 1}',
+          style: TextStyle(
+              fontSize: isSelected ? 20 : 18,
+              color: isSelected ? Colors.white : Colors.black),
+        ),
+      ),
     );
   }
 }
-
-// class XButton extends StatelessWidget {
-//   const XButton({
-//     required this.key,
-//     required this.index,
-//   }) : super(key: key);
-//   final GlobalKey key;
-//   final int index;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//         height: 100,
-//         width: 80,
-//         color: Colors.blue,
-//         child: Button(key: key, index: index));
-//   }
-// }
-
-// class Button extends StatelessWidget {
-//   const Button({
-//     required this.key,
-//     required this.index,
-//   }) : super(key: key);
-//   final GlobalKey key;
-//   final int index;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print(key);
-//     Future.delayed(const Duration(seconds: 1));
-//     final RenderBox renderBox =
-//         key.currentContext!.findRenderObject() as RenderBox;
-//     final Size size = renderBox.size;
-//     final Offset offset = renderBox.localToGlobal(Offset.zero);
-//     return GestureDetector(
-//       onTap: () {
-//         print('\npressed');
-//         final RenderBox renderBox =
-//             key.currentContext?.findRenderObject() as RenderBox;
-
-//         final Size size = renderBox.size; // or _widgetKey.currentContext?.size
-
-//         // print('Size: ${size.width}, ${size.height}');
-
-//         final Offset offset = renderBox.localToGlobal(Offset.zero);
-
-//         // print('Offset: ${offset.dx}, ${offset.dy}');
-
-//         // print(
-//         //     'Position: ${(offset.dx + size.width) / 2}, ${(offset.dy + size.height) / 2}');
-//         print('xf ${offset.dx} xe ${offset.dx + size.width}');
-//       },
-//       child: Container(
-//         height: 100,
-//         width: 80,
-//         margin: const EdgeInsets.all(5),
-//         color: Colors.orange,
-//       ),
-//     );
-//   }
-// }
 
 List<OffsetDetails>? offsetBoxes = List.filled(
     keys.length, OffsetDetails(xf: null, xe: null, yf: null, ye: null));
