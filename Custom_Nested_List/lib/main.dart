cc495ea5e53bdd40b7b758060136e329eb996c27
remon <blackshadowsoftwareltd@gmail.com>
2022-05-12 01:45:50 +0600

    nested list. not complete

--- Custom_Nested_List/lib/main.dart
+++ Custom_Nested_List/lib/main.dart
@@ -16,26 +16,32 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Nested List')),
-        body: Consumer<ListProvider>(
-          builder: (_, state, __) {
+        body: LayoutBuilder(builder: (context, constraint) {
+          return Consumer<ListProvider>(builder: (_, state, __) {
             return ListView.builder(
                 itemCount: state.listOfData.length,
                 itemBuilder: (context, index) {
                   final _listOfData = state.listOfData[index];
-                  return MainTile(
-                      data: _listOfData, selectedModel: state.selectedModel);
+                  return Tile(
+                      constraint: constraint,
+                      data: _listOfData,
+                      selectedModel: state.selectedModel);
                 });
-          },
-        ));
+          });
+        }));
   }
 }
 
-class MainTile extends StatelessWidget {
-  const MainTile({Key? key, required this.data, required this.selectedModel})
+class Tile extends StatelessWidget {
+  const Tile(
+      {Key? key,
+      required this.data,
+      required this.selectedModel,
+      required this.constraint})
       : super(key: key);
   final Model data;
   final Model? selectedModel;
-
+  final BoxConstraints constraint;
   @override
   Widget build(BuildContext context) {
     final _width = MediaQuery.of(context).size.width;
@@ -48,22 +54,40 @@ class MainTile extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(
-                width: _width,
-                child: OutlinedButton(
-                    onPressed: () {
-                      print('pressed');
-                      state.selectedModel = data;
-                    },
-                    child: Text(data.index.toString())),
-              ),
+                  width: _width,
+                  child: OutlinedButton(
+                      onPressed: () {
+                        state.selectedModel = data;
+                      },
+                      child: Text(data.index.toString()))),
               Container(
-                height: data == selectedModel ? 100 : 0,
+                height: data == selectedModel ? constraint.maxHeight : 0,
                 color: Colors.green,
+                child: ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: data.values.length,
+                    itemBuilder: (context, i) {
+                      final _subData = data.values[i];
+                      return SubTile(data: _subData);
+                    }),
               )
             ],
           ),
         );
       },
     );
+  }
+}
+
+class SubTile extends StatelessWidget {
+  const SubTile({Key? key, required this.data}) : super(key: key);
+  final int data;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+        padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+        color: Colors.white,
+        child: Text(data.toString()));
   }
 }
--- Custom_Nested_List/lib/provider.dart
+++ Custom_Nested_List/lib/provider.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/cupertino.dart';
 
 class ListProvider extends ChangeNotifier {
-  final listOfData = List<Model>.generate(
-      100, (i) => Model(index: i, values: List.generate(100, (x) => x)));
+  final listOfData = List<Model>.generate(100, (i) {
+    List<int> x = List.generate(100, (x) => x);
+    x.shuffle();
+
+    return Model(index: i, values: x);
+  });
 
   Model? _selectedModel = null;
   Model? get selectedModel => _selectedModel;
 
   set selectedModel(Model? data) {
-    _selectedModel = data;
+    if (data == selectedModel) {
+      _selectedModel = null;
+    } else {
+      _selectedModel = data;
+    }
     notifyListeners();
   }
 }
