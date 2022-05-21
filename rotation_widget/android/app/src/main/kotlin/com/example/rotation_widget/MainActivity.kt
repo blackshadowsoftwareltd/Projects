1ab3f50dc1b7965686aafbd26b6d4f1997ebeea5
remon <blackshadowsoftwareltd@gmail.com>
2022-05-22 01:39:51 +0600

    -

---
+++ rotation_widget/android/app/src/main/kotlin/com/example/rotation_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rotation_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rotation_widget/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AppBar')),
+    );
+  }
+}
