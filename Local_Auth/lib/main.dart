c3f427a34843ab6b297280d33a7a1a6a0c2cb107
remon <blackshadowsoftwareltd@gmail.com>
2022-06-05 23:04:03 +0600

    package

--- Local_Auth/lib/main.dart
+++ Local_Auth/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(const MaterialApp(home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('AppBar')),
+    );
   }
+}
+
+enum SupportedState {
+  unknown,
+  supported,
+  unsupported,
 }
--- Local_Auth/windows/flutter/generated_plugins.cmake
+++ Local_Auth/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  local_auth_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
