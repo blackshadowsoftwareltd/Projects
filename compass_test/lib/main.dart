16824612cab8ee983da2e97cf0e79ec9d1f0ff81
remon <blackshadowsoftwareltd@gmail.com>
2022-06-09 22:21:54 +0600

    compass init

---
+++ compass_test/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Compass App'),
+      ),
+    );
+  }
+}
